9ccda18ec6a4f23e333e9fcd2f6318b39d4d0255
beauty0 <isigirluk@yahoo.co.uk>
2021-12-11 16:28:08 +0000

    Add project files.

---
+++ PIMFIx/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PIMFIx.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
